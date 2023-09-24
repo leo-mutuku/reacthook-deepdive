3bc6c9acb8dad2442def167681ca2f03a38d64da
leo-mutuku <le.mutuku@gmail.com>
2023-09-24 10:13:01 +0300

    usestate with objects

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import './App.css'
 import ClassCounter from './components/ClassCounter'
 import FunctionalCounter from './components/FunctionalCounter'
 import HookCounter from './components/HookCounter'
+import HookCounter1 from './components/HookCounter1'
 function App() {
   return (
     <>
     {/* <ClassCounter />
     <FunctionalCounter /> */}
     <HookCounter />
+    <HookCounter1 />
     </>
   )
 }
