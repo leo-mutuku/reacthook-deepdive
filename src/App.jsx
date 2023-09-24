a087f4d748ebb67666065de90652aadac1b139ce
leo-mutuku <le.mutuku@gmail.com>
2023-09-24 09:33:00 +0300

    useState with previous value

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@
 import './App.css'
 import ClassCounter from './components/ClassCounter'
 import FunctionalCounter from './components/FunctionalCounter'
+import HookCounter from './components/HookCounter'
 function App() {
   return (
     <>
-    <ClassCounter />
-    <FunctionalCounter />
+    {/* <ClassCounter />
+    <FunctionalCounter /> */}
+    <HookCounter />
     </>
   )
 }
