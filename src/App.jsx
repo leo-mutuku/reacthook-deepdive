8c112c3e4c578549d31b20a31d53c111e1f6f6d9
leo-mutuku <le.mutuku@gmail.com>
2023-09-24 08:49:11 +0300

    simple button counter - class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 
 
 import './App.css'
+import ClassCounter from './components/ClassCounter'
 function App() {
   return (
     <>
-    
+    <ClassCounter />
     </>
   )
 }
---
+++ src/components/ClassCounter.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+class ClassCounter extends React.Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            count:0
+        }
+    }
+
+    incrementCounter = () =>{
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
+  render() {
+    return <div>
+        <button onClick={this.incrementCounter}>Count {this.state.count}</button>
+    </div>;
+  }
+}
+
+export default ClassCounter;
